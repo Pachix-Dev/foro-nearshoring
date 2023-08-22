af6d7b911e84f540d88b7c00f74aa2a0b7bc82a6
Pachi-Dev <pachi.claros@gmail.com>
2023-08-22 11:48:37 -0600

    config project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
+import { Register } from './components/Register'
+import './App.css'
+import { Pass } from './components/Pass'
+import { Container } from 'react-bootstrap'
+import { Verify } from './components/Verify'
+
+export function App () {
+  return (
+    <>
+      <div className='foro-nearshowring'>
+        <div className='wrapper-main-foro  pt-5 pb-5'>
+          <Routes>
+            <Route path='/' element={<Register />} />
+            <Route path='/registro-gratis' element={<Pass />} />
+            <Route path='/verify/:qr' element={<Verify />} />
+          </Routes>
+        </div>
+      </div>
+      <footer className=''>
+        <Container>
+          <div className='d-inline-block text-center'>
+            <img src='/foro-nearshowring/igecoLogo.webp' alt='igeco' width={300} />
+            <img src='/foro-nearshowring/GTO 200 WT.webp' alt='gto-200años' width={130} />
+          </div>
+        </Container>
+      </footer>
+    </>
+  )
+}
