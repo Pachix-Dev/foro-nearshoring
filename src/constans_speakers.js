7bcbdd8fba153b88b7966a974c42101a5e6f5fb0
Pachi-Dev <pachi.claros@gmail.com>
2023-08-31 16:31:28 -0600

    add more speakers

--- src/constans_speakers.js
+++ src/constans_speakers.js
@@ -1,13 +1,32 @@
 const speakers = [
     {
-        id: 7,
+        id: 1,
         conference: '',
         name: 'José Guillermo Zozaya Délano',
         company: 'Mexicana de la Industria Automotriz',
         rol: 'Presidente Ejecutivo de la Asociación',
         avatar: '/foro-nearshoring/speakers/JoseGuillermo.webp',
         sketch: 'El Dr. Zozaya es Presidente Ejecutivo de la Asociación Mexicana de la Industria Automotriz a partir de Agosto del 2020. Fungió como Presidente, Gerente General y Representante Ejecutivo de Kansas City Southern de México, S.A. de C.V. (KCSM),empresa subsidiaria de Kansas City Southern (KCS) (NYSE: KSU), desde Abril del 2006 a Julio del 2020. Cuenta con más de 40 años de experiencia en relaciones gubernamentales, asuntos legales y asuntos internacionales, así como en fusiones y adquisiciones. Previo a ser Presidente Ejecutivo de Kansas City Southern de México, S.A. de C.V. (KCSM), fue Director Jurídico y de Relaciones Gubernamentales de ExxonMobil México, posición que ocupó durante nueve años. Tiene un Doctorado Honoris Causa otorgado por parte de la Academia Mexicana de Derecho Internacional y una Licenciatura en Derecho por la Universidad Iberoamericana. Realizó estudios avanzados en Derecho Corporativo y Competencia Económica en el Instituto Tecnológico Autónomo de México, cursó el programa ejecutivo de Administración de Empresas Internacionales en la Universidad Thunderbird en Arizona, el programa de gestión para abogados en la Universidad de Yale y el curso ejecutivo para miembros del consejo de administración en la Escuela de Negocios de    Harvard. Fue también Presidente de la Asociación Mexicana de Ferrocarriles para el periodo 2020 – 2021; Vicepresidente de CONCAMIN desde 2018, miembro del Consejo de Administración de Afore Siglo XXI, de la empresa Ferrovalle y de Cumbre de Negocios. Desde el 24 de mayo de 2016, fue nombrado Presidente del Consejo Binacional de Directores de la US-Mexico Chamber of Commerce. Fue Presidente del Consejo Mexicano del Transporte (2014 – 2016), Vicepresidente del Consejo Ejecutivo de Empresas Globales (2010 – 2013), Presidente Nacional de la American Chamber durante dos periodos (2010 – 2012), es miembro del Patronato de la Orquesta Sinfónica del Estado de México y miembro del board de Fomento Educacional A.C.'
+    },
+    {
+        id: 2,
+    conference: '',
+    name: 'Armando Cortés Galicia',
+    company: 'Industria Nacional de Autopartes, A.C.',
+    rol: 'Director General ',
+    avatar: '/foro-nearshoring/speakers/ArmandoCortes.webp',
+    sketch: 'Armando Cortés Galicia es Director General de la Industria Nacional de Autopartes, A.C. (INA) desde junio de 2023. Es especialista en la industria automotriz y cuenta con una trayectoria profesional de más de 15 años en los sectores público y privado transnacional. Fue Director Ejecutivo de la Industria Automotriz y de Autopartes en ProMéxico, siendo integrante del equipo responsable de la atracción al país de las plantas de BMW en San Luis Potosí, KIA en Nuevo León y Toyota en Guanajuato, además de muchos otros proyectos de inversión relevantes en la industria automotriz terminal y en la cadena de suministro, en un periodo en el que se registró un número inédito de inversiones de plantas automotrices y nuevas líneas de producción en México. En esta responsabilidad, también coordinó la organización de numerosos eventos B2B con la Organización de Comercio Exterior de Japón JETRO, la Embajada de Canadá, así como diversos eventos de promoción con Business France y la Cámara Mexicano-Alemana de Comercio e Industria CAMEXA, entre otros. Anteriormente, se desempeñó como Gerente de Proyectos Estratégicos en Chrysler de México, siendo parte del equipo responsable de la atracción al país de las nuevas líneas de producción del Fiat 500 y la RAM ProMaster. Además, participó en el proyecto del primer vehículo eléctrico manufacturado en México, el Fiat 500 eléctrico, mismo que fue galardonado por el Consejo Nacional de Ciencia y Tecnología. Es autor de diversas publicaciones sobre la industria automotriz y de autopartes, promotor del desarrollo de cadenas de proveeduría a partir de la Industria 4.0 y ha sido conferencista en eventos nacionales e internacionales sobre oportunidades de negocios en el sector. Asimismo, se ha desempeñado como consultor de estrategia y negocios internacionales brindando asesoría en comercio exterior, y recientemente colaboró con la empresa canadiense Trade Export Canada, nombrada una de las 100 start-ups tecnológicas más importantes de Canadá en 2021. Armando es licenciado en Economía, cuenta con estudios de maestría en Prospectiva Estratégica por el Instituto Tecnológico y de Estudios Superiores de Monterrey y un diplomado en Innovación Gubernamental por la Escuela de Gobierno John F. Kennedy de la Universidad de Harvard.'
+    },
+    {
+        id: 3,
+    conference: '',
+    name: ' Claudia Esteves Cano',
+    company: 'Asociación Mexicana de Parques Industriales Privados, A.C. (AMPIP)',
+    rol: 'Directora General',
+    avatar: '/foro-nearshoring/speakers/ClaudiaEsteves.webp',
+    sketch: 'Cuenta con una destacada trayectoria profesional de más de 33 años, siempre comprometida con el desarrollo de México. Ha ocupado cargos en el sector bancario y de promoción internacional en Bancomext y ProMéxico, en áreas de crédito, diseño de programas financieros y promoción de comercio e inversión extranjera en México, desempeñando cargos como Consejero Comercial de México en Italia; Consejero Comercial Adjunto en Alemania; Directora Estatal en Veracruz; y Directora Ejecutiva de Estrategia en la Unidad de Inteligencia de Negocios, entre otros. Asimismo, destaca su participación como miembro del equipo negociador para la modernización del TLCAN/TMEC y su papel como miembro en distintos consejos y comités. Ha impartido conferencias de relevancia internacional y cuenta con diversas publicaciones sobre las industrias estratégicas en México. Actualmente, es la Directora General de la Asociación Mexicana de Parques Industriales Privados, A.C. (AMPIP), organismo que representa uno de los pilares del desarrollo económico del país.'
     }
+
 ]
   export { speakers }
   
