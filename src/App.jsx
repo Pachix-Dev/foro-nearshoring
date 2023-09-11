2fa6cdbd34db292d88c2343269b8cfa9011254e8
JoseZR <josezerrod@hotmail.com>
2023-09-11 00:36:09 -0600

    ajuste de logos en footer

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,17 @@ export function App () {
         </div>
       </div>
       <footer className=''>
-        <Container>
+        {/* <Container>
           <div className='d-inline-block text-center'>
             <img src='/foro-nearshoring/igecoLogo.webp' alt='igeco' width={300} />
             <img src='/foro-nearshoring/GTO 200 WT.webp' alt='gto-200años' width={130} />
+          </div>
+        </Container> */}
+        <Container>
+          <div class='d-flex bd-highlight'>
+            <div class=' bd-highlight' style={{ width: '200px' }}><img src='/foro-nearshoring/igeco_bn.webp' alt='igeco' className='w-100' /></div>
+            <div class=' bd-highlight' style={{ width: '200px' }}><img src='/foro-nearshoring/deutsche_messe_bn.webp' alt='igeco' className='w-100' /></div>
+            <div class=' ms-auto' style={{ width: '200px' }}><img src='/foro-nearshoring/logo-gto.webp' alt='gto-200años' className='w-100' /></div>
           </div>
         </Container>
       </footer>
